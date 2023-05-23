4c7c89cfb3e607576f43a3d251190fabbefbadb0
Frostorik <116587935+Frostorik@users.noreply.github.com>
2023-05-23 12:45:08 +0400

    Изменения в urls.py

--- Mountain/urls.py
+++ Mountain/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from Mountain.views import SubmitData, PerevalAddedDetail, PerevalAddedUpdate
+from Mountain.views import SubmitData, PerevalAddedDetail, PerevalAddedUpdate, PerevalAddedList
 
 urlpatterns = [
-    path('submit-data/', SubmitData.as_view(), name='submit-data'),
+    path('submitData/', SubmitData.as_view(), name='submit-data'),
     path('submitData/<int:pk>/', PerevalAddedDetail.as_view(), name='pereval-detail'),
     path('submitData/<int:pk>/update/', PerevalAddedUpdate.as_view(), name='pereval-update'),
+    path('submitData/email/', PerevalAddedList.as_view(), name='pereval-list'),
 ]
